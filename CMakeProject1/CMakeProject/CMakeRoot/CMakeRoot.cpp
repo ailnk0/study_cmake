aea3265c571820e2e63aac5dd7940e6243009440
Benedict <ailnk@naver.com>
2023-12-21 17:04:22 +0900

    initial proj

---
+++ CMakeProject1/CMakeProject/CMakeRoot/CMakeRoot.cpp
@@ -0,0 +1,12 @@
+﻿// CMakeProject1.cpp : Defines the entry point for the application.
+//
+
+#include "CMakeRoot.h"
+
+using namespace std;
+
+namespace cmakeroot {
+
+void CMakeRoot::printMsg() { std::cout << "cmakeroot" << std::endl; }
+
+}  // namespace cmakeroot
---
+++ CMakeProject1/CMakeProject/CMakeRoot/CMakeRoot.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <iostream>
+
+namespace cmakeroot {
+
+class CMakeRoot {
+ public:
+  void printMsg();
+};
+
+}  // namespace cmakeroot
